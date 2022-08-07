4f212d26b643aecf8ca07024ec6a831aef583069
harperjohn <jrogers@of-one.com>
2022-08-07 14:36:42 -0400

    update

---
+++ parts/1-getting-started/ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Data;
+
+public class ContosoPizzaContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+
+    public string DbPath { get; }
+    public ContosoPizzaContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    }
+}
